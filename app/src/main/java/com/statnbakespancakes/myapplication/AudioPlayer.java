b34b8e545b079df2ad827eba81bdb135ba238de6
Vladimir <51519538+gegemonTV@users.noreply.github.com>
2021-04-22 23:51:14 +0300

    init

---
+++ app/src/main/java/com/statnbakespancakes/myapplication/AudioPlayer.java
@@ -0,0 +1,27 @@
+package com.statnbakespancakes.myapplication;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+public class AudioPlayer {
+    private MediaPlayer mPlayer;
+    public void stop(){
+        if (mPlayer != null){
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
+
+    public void play(Context c){
+        stop();
+
+        mPlayer  = MediaPlayer.create(c, R.raw.music);
+        mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                stop();
+            }
+        });
+        mPlayer.start();
+    }
+}
---
+++ app/src/main/java/com/statnbakespancakes/myapplication/MainActivity.java
@@ -0,0 +1,37 @@
+package com.statnbakespancakes.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AudioPlayer mPlayer = new AudioPlayer();
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction()
+                .setReorderingAllowed(true)
+                .add(R.id.fragmentContainerView, Christmas.class, null)
+                .commit();
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPlayer.play(getApplicationContext());
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mPlayer.stop();
+    }
+}
